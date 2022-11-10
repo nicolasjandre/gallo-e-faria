d3e980787f501bfc8ea74442cb2751aedf8c5da7
Nicolas Jandre <nicolasjandre@live.com>
2022-11-10 07:57:57 -0300

    adding slide changing with javascript

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-/* Test */
+/* Slide changing */
+
+
+/* FOI CRIADA UMA VARIAVEL COUNT COMEÇANDO PELO 1
+ESSA VARIAVEL SE REFERE AO INPUT RADIO QUE CRIAMOS
+O DOCUMENT.CHECKED = TRUE INICIAL É PARA DEIXAR DE PADRAO
+A PRIMEIRA RADIO ATIVA AO ABRIR A PÁGINA
+O SET INTERVAL TEM A FUNCTION NEXTIMAGE DENTRO DELE
+E OS 4S DE TEMPO PRA TROCA DO SLIDE
+
+COUNT++ É PARA SUBIR SEMPRE UM NÚMERO DA VARIAVEL COUNT
+SE O COUNT FOR MAIOR QUE 4, A COUNT VOLTA PRA 1
+USAMOS NO FINAL A VARIAVEL COUNT PRA SETAR O RADIO EM ESPECIFICO QUE
+QUEREMOS ATIVO */
+
+
+let count = 1
+document.getElementById("radio1").checked = true;
+
+setInterval( function(){
+    nextImage();
+}, 4000)
+
+function nextImage(){
+    count++;
+    if(count > 4) {
+        count = 1;
+    }
+
+    document.getElementById("radio" + count).checked = true;
+}
